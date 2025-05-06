d5162f3835d65a9841b7f2af895a8bb26fd6be83
emillyme <emill.ymello212@gmail.com>
2025-05-06 17:00:06 -0300

    updat

---
+++ src/components/InputGeral.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps({
+        nomeLabel: String,
+        type: String
+    })
+</script>
+
+<template>
+    <label for="">{{ nomeLabel }}</label>
+    <input class="border border-gray-300 rounded w-[350px]" type="type">
+</template>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,31 @@
+<script setup>
+import InputGeral from '@/components/inputGeral.vue';
+
+</script>
+
+<template>
+  <main class="bg-gray-100 w-screen h-screen flex justify-center">
+    <div class="flex bg-white w-screen my-10 mx-10 ">
+      <div class="w-[800px]">
+        <img src="/src/assets/coffeebg.jpg" class="w-full h-full object-cover">
+      </div>
+
+      <div class="w-full flex justify-center items-center flex-col">
+        <div class="flex justify-center items-center flex-col mb-20">
+          <h1 class="font-medium text-2xl mb-2">Welcome back</h1>
+          <p class="text-gray-500">Our best coffe for your favorite coffee shop!</p>
+        </div>
+        <div class="">
+          <form action="" class="flex flex-col">
+            <InputGeral nomeLabel="Username" type="text"/>
+            <InputGeral nomeLabel="Password" type="password"/>
+            <button>
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </main>
+</template>
